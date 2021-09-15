60a7b53a79329b6dff29303690096f66a7c4c18f
Letlab0611 <83432355+Letlab0611@users.noreply.github.com>
2021-09-15 18:07:40 -0300

    Add files via upload

---
+++ trabalho_daniel_circulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace liçãoDanielMain
+{
+
+    class Program
+    {
+        //declara valor de pi
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            //pede pelo valor do raio e calcula.
+            Console.WriteLine("Entre com valor do raio: ");
+            //envia valores pra outra função
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double circ = Circuferencia(raio);
+            double volume = Volume(raio);
+            //mostra resultados
+            Console.WriteLine("Circuferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        static double Circuferencia(double r)
+        {
+            //calcula circunferencia
+            return 2.0 * Pi * r;
+        }
+        static double Volume(double r)
+        {
+            //calcula volume
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+
+
+}
